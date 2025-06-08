23a593ccf94606948ca77f119b7381f50944c546
codestudioinc <codestudio.service@gmail.com>
2025-06-08 20:28:57 +0530

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,7 +3,7 @@ import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 const footerLinks = [
 	{
 		title: "WhatsApp",
-		href: "#",
+		href: "tel:+919394070912",
 	},
 	{
 		title: "Twitter",
@@ -21,14 +21,14 @@ const footerLinks = [
 
 export const Footer = () => {
 	return (
-		<footer className="relative -z-10 overflow-hidden">
+		<footer className="relative overflow-hidden">
 			<div
 				className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30"
 				style={{
 					maskImage:
-						"radial-gradient(50% 50% at bottom center, black, transparent -z-10)",
+						"radial-gradient(50% 50% at bottom center, black, transparent)",
 					WebkitMaskImage:
-						"radial-gradient(50% 50% at bottom center, black, transparent -z-10)",
+						"radial-gradient(50% 50% at bottom center, black, transparent)",
 				}}
 			></div>
 
@@ -38,9 +38,9 @@ export const Footer = () => {
 					<nav className="flex flex-col md:flex-row items-center gap-8">
 						{footerLinks.map((link) => (
 							<a
-								href="#"
+								href={link.href}
 								key={link.title}
-								className="inline-flex items-center gap-1.5"
+								className="inline-flex items-center gap-1.5 z-50"
 							>
 								<span className="font-semibold">{link.title}</span>
 								<ArrowUpRightIcon className="size-4" />
