ab73f4f43f2c4718e171e534a41d3f29d559b196
codestudioinc <codestudio.service@gmail.com>
2025-07-09 14:24:12 +0530

    Update About.tsx

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -42,51 +42,6 @@ const toolboxItems = [
 	},
 ];
 
-const hobbies = [
-	{
-		title: "Painting",
-		emoji: "🎨",
-		left: "5%",
-		top: "5%",
-	},
-	{
-		title: "Photography",
-		emoji: "📸",
-		left: "50%",
-		top: "5%",
-	},
-	{
-		title: "Hiking",
-		emoji: "🥾",
-		left: "35%",
-		top: "40%",
-	},
-	{
-		title: "Gaming",
-		emoji: "🎮",
-		left: "10%",
-		top: "35%",
-	},
-	{
-		title: "Music",
-		emoji: "🎻",
-		left: "70%",
-		top: "45%",
-	},
-	{
-		title: "Fitness",
-		emoji: "🏋️‍♂️",
-		left: "5%",
-		top: "65%",
-	},
-	{
-		title: "Reading",
-		emoji: "📚",
-		left: "45%",
-		top: "70%",
-	},
-];
-
 export const AboutSection = () => {
 	return (
 		<div id="about" className="py-20 lg:py-28">
@@ -119,46 +74,6 @@ export const AboutSection = () => {
 								className="mt-3"
 								itemsWrapperClassName="-translate-x-1/2"
 							/>
-						</Card>
-					</div>
-					<div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
-						<Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
-							<CardHeader
-								title="Beyond the Code"
-								description="Explore our interests and hobbies beyond the digital realm."
-								className="px-6 py-6"
-							/>
-							<div className="relative flex-1">
-								{hobbies.map((hobby) => (
-									<div
-										key={hobby.title}
-										className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
-										style={{
-											left: hobby.left,
-											top: hobby.top,
-										}}
-									>
-										<span className="font-medium text-gray-950">
-											{hobby.title}
-										</span>
-										<span>{hobby.emoji}</span>
-									</div>
-								))}
-							</div>
-						</Card>
-						<Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
-							<Image
-								src={mapImage}
-								alt="map"
-								className="h-full w-full object-cover object-left-top"
-							/>
-							<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
-								<Image
-									src={smileMemoji}
-									alt="smiling memoji"
-									className="size-20"
-								/>
-							</div>
 						</Card>
 					</div>
 				</div>
